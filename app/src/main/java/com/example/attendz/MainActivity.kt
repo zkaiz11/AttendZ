3b331c921d81e8c3e0844d5700056a9258432128
Kai <kai@Kai-2.local>
2023-10-04 18:28:39 +0700

    google auth implemented and rework on bottom nav bar

--- app/src/main/java/com/example/attendz/MainActivity.kt
+++ app/src/main/java/com/example/attendz/MainActivity.kt
@@ -2,33 +2,25 @@ package com.example.attendz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.auth.FirebaseAuth
-
-import android.content.Intent
-import android.widget.TextView
-import androidx.core.os.bundleOf
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.setFragmentResult
 import com.example.attendz.databinding.ActivityMainBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.squareup.picasso.Picasso;
-import com.example.attendz.Home
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mGoogleSignInClient: GoogleSignInClient
-    private lateinit var mAuth: FirebaseAuth
     private lateinit var binding: ActivityMainBinding
     private lateinit var fragmentManager: FragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
@@ -45,7 +37,6 @@ class MainActivity : AppCompatActivity() {
 
         if (user != null) {
             showFragment(Home())
-//            Picasso.get().load(user.photoUrl).into(imgView);
         } else {
             // Handle the case where the user is not signed in
         }
@@ -70,33 +61,9 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-
-
-        // Inside onCreate() method
-//        val sign_out_button = findViewById<Button>(R.id.logout_button)
-//        sign_out_button.setOnClickListener {
-//            signOutAndStartSignInActivity()
-//        }
     }
-
-    private val auth = Firebase.auth
-    private val user = auth.currentUser
     private fun showFragment(fragment: Fragment) {
-        val bundle = Bundle()
-        bundle.putString("name", user?.displayName)
-        bundle.putString("imgUrl", user?.photoUrl.toString())
         fragmentManager = supportFragmentManager
-        fragmentManager.setFragmentResult("user", bundle)
         fragmentManager.beginTransaction().replace(R.id.lytFragment, fragment).commit()
-    }
-    private fun signOutAndStartSignInActivity() {
-        mAuth.signOut()
-
-        mGoogleSignInClient.signOut().addOnCompleteListener(this) {
-            // Optional: Update UI or show a message to the user
-            val intent = Intent(this@MainActivity, SignInActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
     }
 }
