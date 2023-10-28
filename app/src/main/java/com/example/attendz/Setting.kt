7c959649c87d0f4fb1ce24957c81b70b03672532
Chandara H. Wei <gdarademoire2003@gmail.com>
2023-10-28 16:01:45 +0700

    update

--- app/src/main/java/com/example/attendz/Setting.kt
+++ app/src/main/java/com/example/attendz/Setting.kt
@@ -1,16 +1,19 @@
 package com.example.attendz
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.app.ActivityCompat.recreate
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
+import java.util.Locale
 
 
 class Setting : Fragment() {
@@ -31,6 +34,27 @@ class Setting : Fragment() {
         }
         return view
     }
+
+    //navigate to khmer language
+    fun onLanguageSwitchClick(view: View?) {
+        // Change the app's locale to the second language
+        val newLocale = Locale("km")
+        Locale.setDefault(newLocale)
+
+        // Update the configuration and resources for the new locale
+        val config = Configuration()
+        config.setLocale(newLocale)
+        resources.updateConfiguration(config, resources.displayMetrics)
+
+        // Restart the activity to apply the new language
+        recreate()
+    }
+
+    private fun recreate() {
+        TODO("Not yet implemented")
+    }
+
+
     private fun signOutAndStartSignInActivity() {
         Firebase.auth.signOut()
         val intent = Intent(activity, SignInActivity::class.java)
---
+++ app/src/main/java/com/example/attendz/SettingActivity.java
@@ -0,0 +1,4 @@
+package com.example.attendz;
+
+public class SettingActivity {
+}
